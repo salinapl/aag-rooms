b406cd4e5d245714af7a9988fb7e26b58e4b313e
Swizzler121 <36831696+Swizzler121@users.noreply.github.com>
2026-03-19 14:39:42 -0500

    add javascript onclick event to template

--- templates/room.php
+++ templates/room.php
@@ -50,7 +50,7 @@
                 <div class="description"> <?= $page->description()->kirbytext() ?></div>
                 <?php if ($page->pgtouch()->bool()): ?>
                     <p>Press to reserve room:
-                    <a class="aag-button" href="<?= $page->pgbutton()->url() ?>"> Reserve </a>
+                    <a class="aag-button" href="" onclick="scheduledoc(<?= $page->pgbutton()->url() ?>)"> Reserve </a>
                     </p>
                 <?php else: ?>
                     <div class ="aag-qr">
